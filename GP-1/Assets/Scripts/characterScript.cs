94deca93c9e092b7a95ada1d1d16a5da4ec07dec
kaan-kale <kaan-kale@noreply.com>
2020-04-15 05:51:36 +0300

    Exp in desc.
    Added placeholder assets.
    Added placeholder tilemaps.
    Added placeholder run animation.
    Got moving working.
    Rebuilt the solution for game scripts.
    Experiments with area loading and collisions.

---
+++ GP-1/Assets/Scripts/characterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterScript : MonoBehaviour
+{
+    // RB is defined.
+    public Rigidbody2D rb;
+    //Integers are defined.
+    public int speed;
+    //Animator is defined.
+    public Animator animator;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"))*speed;
+        
+        if(rb.velocity.x!=0||rb.velocity.y!=0)
+        {
+            animator.SetBool("isRunning",true);
+        }
+        else
+        {
+            animator.SetBool("isRunning",false);
+        }
+    }
+}
